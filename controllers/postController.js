92fd770733f8ac89b50767074381521730c3a990
“ElisaGalasso“ <galassoelisa72@gmail.com>
2025-11-06 18:28:40 +0100

    create connection  with db on mysql and implement query index

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,9 +1,15 @@
-const posts = require('../data/posts')
+const posts = require('../routers/posts')
+const connection = require('../data/dbPost')
 
 //index
 function index(req, res) {
     //res.send('Show all posts')
-    res.json(posts)
+
+    const sql = 'SELECT * FROM posts'
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err.message })
+        res.json(results)
+    })
 }
 
 //show
